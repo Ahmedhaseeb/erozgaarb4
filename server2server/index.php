ddece7c54fab3b317b0ffe9c02c3e6e55fa85636
Ahmedhaseeb <ahmedhaseeb123@gmail.com>
2018-12-11 11:33:12 +0500

    server2server direct download
    server2server direct download

---
+++ server2server/index.php
@@ -0,0 +1,18 @@
+	<?php
+		set_time_limit(0);
+		$filename = "file_name_here"; // your file will be save with this name example: abc.zip
+		$download_from = "link_of_file_to_download"; //example: http://abc.com/backup.zip
+		//This is the file where we save the    information
+		$fp = fopen (dirname(__FILE__) . '/salonspa7_new.zip', 'w+');
+		//Here is the file we are downloading, replace spaces with %20
+		$ch = curl_init(str_replace(" ","%20","https://armissalonandspa.com/salonspa7_new.zip"));
+		curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+		// write curl response to file
+		curl_setopt($ch, CURLOPT_FILE, $fp); 
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		// get curl response
+		curl_exec($ch); 
+		curl_close($ch);
+		fclose($fp);
+	?>
+
