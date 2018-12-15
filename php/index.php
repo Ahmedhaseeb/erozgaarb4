bc11637de79d8a6f8ead71c68ef8657d1f812da8
Ahmedhaseeb <ahmedhaseeb123@gmail.com>
2018-12-15 17:01:26 +0500

    PHP Lecture

---
+++ php/index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP - Introduction</title>
+
+
+</head>
+<body>
+
+
+<?php
+
+
+
+
+echo "ahmed" <= "aHmed";
+
+die();
+
+$num1 = 20;
+
+$num2 = 20;
+
+echo $num1 >= $num2;
+
+die();
+
+
+
+$count = 10;
+
+$msg = 'You have ';
+$msg .= $count;
+$msg .= ' New messages';
+
+echo $msg;
+die();
+
+$x = 1;
+
+
+$x %= 10;  // $x = $x % 10;
+
+echo $x;
+
+
+die();
+echo "<h1>";
+echo $x++;
+echo "<br>";
+echo $x;
+
+
+echo "</h1>";
+// $x = $x  + 1;
+
+// echo $x;
+
+
+
+
+
+die();
+
+/*
+
+
+echo "Asdasd";
+print("asdasd");
+
+
+
+*/ 
+	//  this is not going to print on screen
+
+	// 		asdnaklsjdkla dkahsj kdhaskjda
+	// 		sd
+	// 		a sdasdasd
+
+
+	$abc = "20" / "2";
+	
+	// echo $abc;
+
+
+$User_name = "Ahmed";
+$user_name = "haseeb";
+
+// echo $user_name;
+
+// echo 3 % 2;
+
+
+$tictactoe = array(
+	array("0", "X", "X" ),
+	array("X", "0", "X" ),
+	array("0", "0", "X" ),
+	
+
+);
+
+// echo $tictactoe[2][2];
+	// echo $players[3];
+
+
+
+
+
+?>
+</body>
+</html>
