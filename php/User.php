abd4135fd7772c5afbd3a031e07e5e12c4c60163
Ahmedhaseeb <ahmedhaseeb123@gmail.com>
2018-12-31 10:59:47 +0500

    private data members

--- php/User.php
+++ php/User.php
@@ -1,20 +1,26 @@
 <?php
 class User{
-	public $name,$password;
+	private $name,$password;
 
 	function __construct($n="",$p=""){
-
 		$this->name = $n;
 		$this->password = $p;
 	}
 
 
-	function save_user()
+	public function save_user()
+		{
+			echo $this->name . " saved<br>";
+		}
+	public function showName()
 	{
 		echo $this->name;
-		echo "<br>";
-		echo "User Saved";
-		echo "<br>";
+	}
+
+
+	public function setName($name)
+	{
+		$this->name = $name;
 	}
 
 }
--- php/classes.php
+++ php/classes.php
@@ -7,17 +7,31 @@
 </head>
 <body>
 	<?php 
-	$x = 20;
-		
+		$x = 20;
+		$user1 = new User("Ahmed","123");
 
-		$user1 = new User("abcasdasasd","123");
+		$user1->setName("abc");
+		$user1->showName();
+		// $user1->save_user();
+
+		// echo $user1->password;
+
+		$user2 = new User("ABC","456");
+
+		// $user2->save_user();
+
+		// $user1 = new User("abcasdasasd","123");
 		// print_r($user1);
-		$user1->save_user();
+		// $user1->save_user();
 		
-		$user2 = new User("Shoaib","456");
-		$user2->save_user();
+		// $user2 = new User("Shoaib","456");
+		// $user2->save_user();
 		// print_r($user2);
 
+
+
+
+		
 	?>
 </body>
 </html>
