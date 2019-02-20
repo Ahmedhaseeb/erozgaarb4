269844c95ad7d34ea5a227a08948ced4fb53b462
Ahmedhaseeb <ahmedhaseeb123@gmail.com>
2019-02-20 21:03:46 +0500

    Update index.php

--- server2server/index.php
+++ server2server/index.php
@@ -1,11 +1,11 @@
 	<?php
 		set_time_limit(0);
-		$filename = "file_name_here"; // your file will be save with this name example: abc.zip
+		$filename = "file_name_here.zip"; // your file will be save with this name example: abc.zip
 		$download_from = "link_of_file_to_download"; //example: http://abc.com/backup.zip
 		//This is the file where we save the    information
-		$fp = fopen (dirname(__FILE__) . '/salonspa7_new.zip', 'w+');
+		$fp = fopen (dirname(__FILE__) . '/'. $filename, 'w+');
 		//Here is the file we are downloading, replace spaces with %20
-		$ch = curl_init(str_replace(" ","%20","https://example.com/myfile.zip"));
+		$ch = curl_init(str_replace(" ","%20",$download_from));
 		curl_setopt($ch, CURLOPT_TIMEOUT, 50);
 		// write curl response to file
 		curl_setopt($ch, CURLOPT_FILE, $fp); 
